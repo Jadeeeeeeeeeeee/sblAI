9b77c3f7dc790a1a0fb7f9ef01d4d2110cb09407
Jadeeeeeeeeeeee <Yali.shlomi@icloud.com>
2026-04-22 21:10:29 +0300

    added vercel analytics and speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import Navbar from "./navbar.jsx";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 import { WorkoutProvider } from "./context/workoutContext.js";
 export const metadata = {
@@ -10,6 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`h-full`}>
       <body className="min-h-screen flex bg-black flex-col items-center">
+        <SpeedInsights></SpeedInsights>
+        <Analytics></Analytics>
         <WorkoutProvider>
            <Navbar ></Navbar>
            {children}
